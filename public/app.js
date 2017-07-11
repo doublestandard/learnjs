4b715b18b0b843b3236d60b8e6a8ce64dd9acd0d
doublestandard <miyamegu@gmail.com>
2017-07-11 16:00:24 +0900

    2017/07/11分

--- public/app.js
+++ public/app.js
@@ -1,16 +1,55 @@
 'use strict';
 var learnjs = {};
-learnjs.problemView = function(programNumber){
-    var title = 'Problem #' + programNumber + ' Comming Soon!';
-    return  $('<div class="problem-view">').text(title);
+
+learnjs.programs = [
+    {description:"What is truth?",
+    code :"function problem(){return  __}"},
+    {description:"Simple Math",
+        code :"function problem(){return  42 === 6 * __}"}
+];
+
+learnjs.applyObject = function(obj,elem){
+    for(var key in obj){
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+};
+learnjs.problemView = function(data){
+    var programNumber = parseInt(data,10);
+    var view = learnjs.template('problem-view');
+    var programData = learnjs.programs[programNumber - 1];
+    var resultFlash = view.find('.result');
+
+    function checkAnswer(){
+        var answer = view.find('.answer').val();
+        var test = programData.code.replace('__',answer) + '; problem();';
+        return eval(test);
+    }
+
+    function checkAnswerClick(){
+        if(checkAnswer()){
+            var correctFlash = learnjs.buildCorrectFlash(programNumber);
+            learnjs.flashElement(resultFlash,correctFlash);
+        } else {
+            learnjs.flashElement(resultFlash,'Incorrect!');
+        }
+        return false;
+    }
+
+    view.find('.check-btn').click(checkAnswerClick)
+    view.find('.title').text('Problem #' + programNumber)
+    learnjs.applyObject(programData,view);
+    return  view;
 }
 learnjs.showView = function(hash){
     var routes = {
-        '#problem' : learnjs.problemView
+        '#problem' : learnjs.problemView,
+        '#':learnjs.landingView,
+        '':learnjs.landingView
     };
     var hashParts = hash.split('-');
     var viewFn = routes[hashParts[0]];
     if(viewFn){
+        learnjs.triggerEvent('removingView',[]);
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
@@ -20,4 +59,35 @@ learnjs.appOnReady = function(){
         learnjs.showView(window.location.hash);
     };
     learnjs.showView(window.location.hash);
+}
+
+learnjs.flashElement = function(elem,content){
+    elem.fadeOut('fast',function(){
+        elem.html(content);
+        elem.fadeIn();
+    });
+}
+
+learnjs.template = function(name){
+    return $('.templates .' + name).clone();
+}
+
+learnjs.buildCorrectFlash = function (programNum){
+    var correctFlash = learnjs.template('correct-flash');
+    var link =  correctFlash.find('a');
+    if(programNum < learnjs.programs.length){
+        link.attr('href','#problem-' + (programNum + 1));
+    } else {
+         link.attr('href','');
+         link.text("You're Finished!")
+    }
+    return correctFlash;
+}
+
+learnjs.landingView = function () {
+    return learnjs.template('landing-view');
+}
+
+learnjs.triggerEvent = function(name,args){
+    $('.view-container>*').trigger(name,args);
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -8,7 +8,7 @@ describe('LearnJS',function(){
 
     it('can show a problem view',function(){
         learnjs.showView('#problem-1')
-        expect($('.view-container .problem-view').length).toEqual(1);
+        expect($('.view-container').length).toEqual(1);
     });
 
     it('show the landing page view when there is no hash',function(){
@@ -22,18 +22,43 @@ describe('LearnJS',function(){
         expect(learnjs.problemView).toHaveBeenCalledWith('42')
     });
 
-
-    describe('problem view',function(){
-        it('has a title that includes the program number',function(){
-            var view = learnjs.problemView('1')
-            expect(view.text()).toEqual('Problem #1 Comming Soon!')
-        });
-    });
-
     it('subscribes to the hash change event.',function(){
         learnjs.appOnReady();
         spyOn(learnjs,'showView');
         $(window).trigger('hashchange');
         expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
     });
+
+
+    describe('problem view',function(){
+
+        it('has a title that includes the program number',function(){
+            var view = learnjs.problemView('1');
+            expect(view.find('.title').text()).toEqual('Problem #1');
+        });
+        it('show the description',function(){
+            var view = learnjs.problemView('1');
+            expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+         });
+        it('show the program code.',function(){
+            var view = learnjs.problemView('1');
+            expect(view.find('[data-name="code"]').text()).toEqual('function problem(){return  __}');
+         });
+    });
+
+    describe('answer section',function(){
+        it('can check a correct answer by hitting a button.',function(){
+            var view = learnjs.problemView('1');
+            view.find('.answer').val('true');
+            view.find('.check-btn').click();
+             expect(view.find('.result span').text()).toEqual('Correct!');
+        });
+        it('rejects an incorrect answer.',function(){
+            var view = learnjs.problemView('1');
+            view.find('.answer').val('false');
+            view.find('.check-btn').click();
+             expect(view.find('.result').text()).toEqual('Incorrect!');
+        });
+    });
+
 });
